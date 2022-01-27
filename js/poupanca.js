cd6f499e8186cc336b4586b61e42ebd8ff904dc6
Martim <martimcunha@gmail.com>
2022-01-27 20:39:49 -0300

    update

--- js/poupanca.js
+++ js/poupanca.js
@@ -15,21 +15,20 @@ function poupanca() {
         return;
     }
 
-    var taxa1 = (TAXA/12) + 0.005;
-    var calc1 =  (valor1 * taxa1) * periodo;
-    var calc2 = valor1 + calc1;
-    var calc3 = valor1 + (calc2 * taxa1) * periodo;
-    var calc4 = (valor1 - calc3)* (-1);
+    var taxa1 = 0.000530916666666667 + 0.005;
+    var calc1 =  valor1 * taxa1;
+    var calc5 = periodo * calc1;
+    var calc2 = valor1 + calc5;
 
     for (var i = 0; i > periodo; i++) {
         periodo += calc1
     }
 
-    outResposta.textContent = valor1.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    outValorInvestido.textContent = valor1.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 
-    outResposta2.textContent = calc4.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    outRendimento.textContent = calc5.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 
-    outResposta3.textContent = calc3.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    outResposta3.textContent = calc2.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 }
 
 function clearFields() {
